fa7a6a8fd79e5142987c95483228e747665d6a05
Dantraz Biyean Trevizion Tremik <ayushpratap16@gmail.com>
2019-11-16 02:15:37 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+"""
+    Testing code for different neural network configurations.
+    Adapted for Python 3.5.2
+    Usage in shell:
+        python3.5 test.py
+    Network (network.py) parameters:
+        2nd param is epochs count
+        3rd param is batch size
+        4th param is learning rate (eta)
+    """
+
+# ----------------------
+# - read the input data:
+'''
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+'''
+# ---------------------
+# - network.py example:
+#import network
+
+'''
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)

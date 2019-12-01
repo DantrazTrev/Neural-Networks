72562bf8672c9309d85df15a575139c36c468a27
Ayush Pratap Singh <ayushpratap16@gmail.com>
2019-12-02 00:20:47 +0530

    Update network.py

--- network.py
+++ network.py
@@ -71,7 +71,6 @@ def load(filename):
     f = open(filename, "r")
     data = json.load(f)
     f.close()
-    cost = getattr(sys.modules[__name__], data["cost"])
     net = Network(data["sizes"])
     net.weights = [np.array(w) for w in data["weights"]]
     net.biases = [np.array(b) for b in data["biases"]]

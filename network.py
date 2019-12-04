af6b0b88474bc26fd27e55de682277b486d0c995
Aexousia <you@example.com>
2019-12-04 17:20:00 +0530

    Refer the cost function in network file

--- network.py
+++ network.py
@@ -59,6 +59,9 @@ class Network:
         return (output_activations-y)
     def cost_func(a, y):
         return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+    from costfunctions.py import CrossEntropyCost
+    theclass = CrossEntropyCost()
+    return theclass.fn() 
 
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))

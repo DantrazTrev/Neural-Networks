6b0cc579c8de60163d465cfffbc8f97bf9f3f1d5
Aexousia <you@example.com>
2019-12-04 17:24:14 +0530

    Refer the cost function in network file

--- network.py
+++ network.py
@@ -57,14 +57,13 @@ class Network:
 
     def cost_derivative(self, output_activations, y):
         return (output_activations-y)
-    def cost_func(a, y):
-        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+    
     from costfunctions.py import CrossEntropyCost
     theclass = CrossEntropyCost()
     return theclass.fn() 
 
-def sigmoid(z):
-    return 1.0/(1.0+np.exp(-z))
+    def sigmoid(z):
+        return 1.0/(1.0+np.exp(-z))
 
-def sigmoid_prime(z):
-    return sigmoid(z)*(1-sigmoid(z))
+    def sigmoid_prime(z):
+        return sigmoid(z)*(1-sigmoid(z))

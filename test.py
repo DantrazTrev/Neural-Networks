1e52b3eed61f086c01ef4256bf19df3bd1e5a145
Ayush Pratap Singh <ayushpratap16@gmail.com>
2019-12-06 19:44:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,15 +11,13 @@
 
 # ----------------------
 # - read the input data:
-'''
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
-'''
 # ---------------------
 # - network.py example:
 #import network
 
-'''
+
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)

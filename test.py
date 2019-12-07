b9cf72a6122f4d63d7894b4b7c09694ddbd512c2
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2019-12-08 03:17:36 +0530

    added the code to save and load json file

--- test.py
+++ test.py
@@ -16,8 +16,10 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 # ---------------------
 # - network.py example:
-#import network
+import network
 
 
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net.save("mnist.json")
+#net = network.load("mnist.json")

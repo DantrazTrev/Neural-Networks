a2006438e64b0e06dcdfe58f6d9952cb333fb4d2
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2019-12-08 03:13:25 +0530

    added doc

--- backpropogation.py
+++ backpropogation.py
@@ -1,6 +1,22 @@
-from network import Network,sigmoid_prime
+import numpy as np
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    return sigmoid(z)*(1-sigmoid(z))
 #The backpropogation function 
 def backprop(net, x, y):
+        '''
+        This function is used to perform backpropogation in Neural Network.It takes network and training Data as input and returns network and derivative of cost wrt to bias and weightsas numpy array.
+        Argument:
+        net: OBJECT
+        x  : LIST   
+        y  : LIST  
+        Returns:
+        net     : OBJECT  
+        nebla_b : np.array
+        nebla_w : np.array
+        '''
         nabla_b = [np.zeros(b.shape) for b in net.biases]
         nabla_w = [np.zeros(w.shape) for w in net.weights]
         activation = x

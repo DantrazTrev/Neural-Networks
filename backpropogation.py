289fd7171959dd5c2e5d583e814a1e5417295b18
RAVI SINGH <43233175+ravising-h@users.noreply.github.com>
2019-12-08 14:23:01 +0530

    added sigmoid and sigmoid_prime function

--- backpropogation.py
+++ backpropogation.py
@@ -1,4 +1,10 @@
-from network import Network,sigmoid_prime
+import numpy as np
+
+def sigmoid(z):
+return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+return sigmoid(z)*(1-sigmoid(z))
 #The backpropogation function 
 def backprop(net, x, y):
         '''
